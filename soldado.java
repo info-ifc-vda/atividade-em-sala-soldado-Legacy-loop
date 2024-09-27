44071404d6b3931efa1533d61fd1360dbc6fed00
IFC VIDEIRA <ifc.videira@ifc.edu.br>
2024-09-27 08:39:22 -0300

    fazendo o commit

---
+++ soldado.java
@@ -0,0 +1,74 @@
+class Soldado {
+    private String nome;
+    private int posicao;
+    private String arma;
+    public static final int quantSoldado=0;
+
+
+    public Soldado(String nome){
+        this.nome = nome;
+        this.posicao= 0;
+        this.arma= "punho";
+        this.quantSoldado+=1;
+    }
+    
+    public void movimentar(int distancia){
+        setPosicao(distancia);
+        System.out.println("o soldado percorreu "+ distancia+" posicoes");
+    }
+
+    public void movimentar(){
+        setPosicao(10);
+        System.out.println("o soldado percorreu 10 posicoes");
+    }
+
+    public void atacar(){
+        if(tamanhoExercito()){
+            setArma("fuzil");
+            System.out.println("O soldado "+this.nome+ " está atacando com o"+ this.arma);
+        }else{
+            System.out.println(" ainda não, esperando o numero do exercito ficar maior");
+        }
+    }
+
+    public void atacar(String arma){
+        if(tamanhoExercito()){
+            setArma(arma);
+            System.out.println("O soldado "+this.nome+ " está atacando com o"+ this.arma);
+        }else{
+            System.out.println(" ainda não, esperando o numero do exercito ficar maior");
+        }
+    }
+
+    public boolean tamanhoExercito(){
+        return this.quantSoldado>=10;
+    }
+
+    // get e sets
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public void setArma(String arma){
+        this.arma= arma;
+    }
+
+    public void setPosicao(int distancia){
+        this.posicao+= distancia; 
+    }
+
+
+    // pensar sobre o controle de instância
+
+    //pensar a sobrecarga
+
+    // public void movimentar
+
+    // public void atacar
+
+    //gets e sets
+}
